6eda951dabe639f64d18a9da33df06be44a78152
Mer <yjieqiong@gmail.com>
2018-08-26 11:18:50 +0800

    App Swoosh 08 - Programmatic segues

---
+++ App Swoosh/App Swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,34 @@
+//
+//  LeagueVC.swift
+//  App Swoosh
+//
+//  Created by Jieqiong Yu on 7/18/18.
+//  Copyright © 2018 Jieqiong Yu. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- App Swoosh/README.md
+++ App Swoosh/README.md
@@ -1,5 +1,5 @@
-#  App Swoosh
-A demo app which includes
-* Welcome Screen
-    * How to make border for button
+#  App Swoosh - 08
+Adding segues in programmatically way
+* Instead of creating segue from button to a new viewcontroller, it creates segue from view controller to another view controller. 
+* performSegue / withIdentifier
 
